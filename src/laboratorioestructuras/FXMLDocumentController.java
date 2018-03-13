dc9e51c4ba416b39aaae6612b56b7540ca887279
Telecentro <Telecentro@Servidor>
2018-03-13 13:24:02 -0500

    traducción

--- src/laboratorioestructuras/FXMLDocumentController.java
+++ src/laboratorioestructuras/FXMLDocumentController.java
@@ -30,6 +30,6 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }     
     
 }

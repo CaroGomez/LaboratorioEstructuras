e105ac9fc1be17b8028a47b859d59613a0bb8992
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2018-03-14 16:26:09 -0500

    dao

---
+++ src/co/edu/udea/edatos/laboratorio1/modelo/dao/ConductoresDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.edatos.laboratorio1.modelo.dao;
+
+import co.edu.udea.edatos.laboratorio1.modelo.Conductor;
+
+/**
+ *
+ * @author Carolina
+ */
+public interface ConductoresDAO {
+    
+    public Conductor consultarConductor(String id);
+    
+}

0fb920adc2157b023b3b0512b82f9ad6176e77e9
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2018-03-18 14:31:04 -0500

    -_-

--- src/co/edu/udea/edatos/laboratorio1/controller/IngresoConductorController.java
+++ src/co/edu/udea/edatos/laboratorio1/controller/IngresoConductorController.java
@@ -31,13 +31,6 @@ public class IngresoConductorController {
         this.stagePrincipal = stagePrincipal;
     }
     
-<<<<<<< HEAD
-=======
-    /**
-     * Initializes the controller class.
-     */
->>>>>>> 75e5bb63a0496b8cbcddae44a29f882c27be5d08
-    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
     @FXML // URL location of the FXML file that was given to the FXMLLoader

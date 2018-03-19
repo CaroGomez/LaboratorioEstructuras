d0e7585c763bbfacebe4b72e699ef11b9a75cfdb
Jefry <Jefry@192.168.1.61>
2018-03-18 19:02:28 -0500

    ventana reportes

--- src/co/edu/udea/edatos/laboratorio1/controller/ReportesController.java
+++ src/co/edu/udea/edatos/laboratorio1/controller/ReportesController.java
@@ -24,16 +24,10 @@ public class ReportesController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // fx:id="tabPropietario"
-    private Tab tabPropietario; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tabCondunctor"
-    private Tab tabCondunctor; // Value injected by FXMLLoader
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert tabPropietario != null : "fx:id=\"tabPropietario\" was not injected: check your FXML file 'Reportes.fxml'.";
-        assert tabCondunctor != null : "fx:id=\"tabCondunctor\" was not injected: check your FXML file 'Reportes.fxml'.";
+       
 
     }
 }
